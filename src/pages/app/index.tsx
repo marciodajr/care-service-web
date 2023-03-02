c1f4d050124527d744b94206c4ef241e3055910c
Marcio dos Anjos Junior <marciodosanjosjunior@gmail.com>
2023-03-02 10:50:33 -0300

    refactor: use timestamp from server

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,21 +1,13 @@
-import { useEffect, useState } from 'react';
 import './styles.css';
 
 export const App = () => {
-  const [timestamp, setTimestamp] = useState('');
-
-  useEffect(() => {
-    const ts = new Date().toString();
-    setTimestamp(ts);
-  }, [])
-
   return (
     <div className="app">
       <h2>
         Service IP Address
       </h2>
       <h2 className="ip">[INTERNAL_IP]</h2>
-      <h4>{timestamp}</h4>
+      <h4>[INTERNAL_TIMESTAMP]</h4>
     </div>
   );
 }
